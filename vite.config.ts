ad7deee4eda2587b27f1279f5382b575898923a3
unknown <tugberkurganci@gmail.com>
2024-11-06 21:19:56 +0300

    deploy time

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-/*
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,9 +12,9 @@ export default defineConfig({
 });
 
 
-*/
 
 
+/*
 export default defineConfig({
   plugins: [react()],
   base: "/xpreia2/",
@@ -25,3 +25,4 @@ export default defineConfig({
     
   },}}
 );
+*/
